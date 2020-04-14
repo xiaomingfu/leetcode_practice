7b743e6dbccaf24b2895d0db30c68737a4f1a7f5
Xiaoming Fu <xmingfu86@gmail.com>
2020-04-13 23:54:38 -0700

    need solve

---
+++ findMaxLength.js
@@ -0,0 +1,2 @@
+// give a binary array, return the max length of a contiguous subarray with same number of  0 and 1
+var findMaxLength = function (nums) {};
---
+++ min_stack.js
@@ -0,0 +1,5 @@
+// design a stack that supports push, pop, top, and retrieving the minmum element in constant time
+
+var MinStack = function () {
+  let minStack = [];
+};
