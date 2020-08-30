8464dc6c6794a811fc66a117d3111f8739f78559
Xiaoming Fu <xmingfu86@gmail.com>
2020-08-29 22:22:12 -0700

    refactor code

--- 485_Max_Consecutive_Ones.js
+++ 485_Max_Consecutive_Ones.js
@@ -1,23 +1,20 @@
+/* 
+Given a binary array, find the maximum number of consecutive 1s in this array.
+*/
+
 var findMaxConsecutiveOnes = function (nums) {
   let cnt = 0;
   let arr = [];
-  let rst = 0;
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === 1) {
       cnt++;
     } else {
-      arr.push(cnt);
-      cnt = 0;
+      if (cnt !== 0) {
+        arr.push(cnt);
+        cnt = 0;
+      }
     }
   }
   arr.push(cnt);
-  let i = 0;
-  for (var j = i + 1; j < arr.length; j++) {
-    if (arr[i] > arr[j]) {
-      continue;
-    } else {
-      i = j;
-    }
-  }
-  return arr[i];
+  return Math.max(...arr);
 };
