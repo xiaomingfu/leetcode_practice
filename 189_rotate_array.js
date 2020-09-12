fc363d1c7b162609c0c8420f5e5be13fdb97a12c
Xiaoming Fu <xmingfu86@gmail.com>
2020-09-12 00:45:46 -0700

    correct and add another solution

--- 189_rotate_array.js
+++ 189_rotate_array.js
@@ -2,9 +2,6 @@
 Given an array, rotate the array to the right by k steps, where k is non-negative.
 */
 var rotate = function (nums, k) {
-  if (k > nums.length) {
-    return;
-  }
   let j = nums.length - 1;
   while (k > 0) {
     let temp = nums[j];
@@ -14,5 +11,10 @@ var rotate = function (nums, k) {
     nums[0] = temp;
     k--;
   }
-  return nums;
 };
+
+// second solution
+
+while (k > 0) {
+  nums.unshift(nums.pop());
+}
