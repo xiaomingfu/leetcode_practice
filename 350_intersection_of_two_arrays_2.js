8be96b08566cc0856ccb1f2754e80dc9edba9ed4
Xiaoming Fu <xmingfu86@gmail.com>
2020-09-12 20:01:17 -0700

    add hash map solution and two pointer solution

--- 350_intersection_of_two_arrays_2.js
+++ 350_intersection_of_two_arrays_2.js
@@ -13,3 +13,61 @@ var intersect = function (nums1, nums2) {
   }
   return res;
 };
+
+// second solution
+
+var intersect = function (nums1, nums2) {
+  let res = [];
+  for (let i = 0; i < nums1.length; i++) {
+    if (nums2.indexOf(nums1[i]) > -1) {
+      res.push(nums1[i]);
+      nums2.splice(i, 1);
+    }
+  }
+  return res;
+};
+
+// sorted two pointers
+var intersect = function (nums1, nums2) {
+  nums1.sort((a, b) => a - b);
+  nums2.sort((a, b) => a - b);
+
+  let i = 0;
+  let j = 0;
+  let res = [];
+  let index = 0;
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] < nums2[j]) {
+      i++;
+    } else if (nums1[i] > nums2[j]) {
+      j++;
+    } else {
+      res[index] = nums1[i];
+      i++;
+      j++;
+      index++;
+    }
+  }
+  return res;
+};
+
+// hash map
+
+var intersect = function (nums1, nums2) {
+  const map = {};
+  const res = [];
+  for (const num1 of nums1) {
+    if (map[num1]) {
+      map[num1]++;
+    } else {
+      map[num1] = 1;
+    }
+  }
+  for (const num2 of nums2) {
+    if (map[num2]) {
+      res.push(num2);
+      map[num2]--;
+    }
+  }
+  return res;
+};
