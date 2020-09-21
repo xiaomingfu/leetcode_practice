e939480c839c36506634d699a56f60f5c6800a15
Xiaoming Fu <xmingfu86@gmail.com>
2020-09-20 22:18:31 -0700

    solve with two pointer and isalnum module

--- 125_valid_palindrome.js
+++ 125_valid_palindrome.js
@@ -1,9 +1,27 @@
 var isPalindrome = function (s) {
-  if (s.length === 0) {
-    return true;
+  let i = 0,
+    j = s.length - 1;
+
+  const isalnum = function (c) {
+    const n = c.charCodeAt(0);
+    return (
+      (n >= "0".charCodeAt(0) && n <= "9".charCodeAt(0)) ||
+      (n >= "a".charCodeAt(0) && n <= "z".charCodeAt(0)) ||
+      (n >= "A".charCodeAt(0) && n <= "Z".charCodeAt(0))
+    );
+  };
+
+  while (i < j) {
+    if (!isalnum(s[i])) {
+      i++;
+    } else if (!isalnum(s[j])) {
+      j--;
+    } else if (s[i].toLowerCase() !== s[j].toLowerCase()) {
+      return false;
+    } else {
+      i++;
+      j--;
+    }
   }
-  let reg = /[A-Z]+/;
-  let res = s.match(reg);
-  console.log(res);
-  for (let i = 0, j = s.length - 1; i <= j; i++, j--) {}
+  return true;
 };
